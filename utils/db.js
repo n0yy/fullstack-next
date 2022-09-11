3ba92c102f2a60bdf66211333f4aa5e0f32156d2
Kaizen <danangpostman37@gmail.com>
2022-09-11 18:39:57 +0700

    export db

--- utils/db.js
+++ utils/db.js
@@ -1,11 +1,11 @@
 const db = require("knex")({
-  client: "mysql",
+  client: process.env.DB_CLIENT,
   connection: {
-    host: "127.0.0.1",
-    port: 3306,
-    user: "root",
-    password: "Script@Kiddies321",
-    database: "fullnext",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   },
 });
 
